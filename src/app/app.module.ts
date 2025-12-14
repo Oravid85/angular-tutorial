62657e90b8949f3813199f287e6e9cf210ebdac7
oravid85 <oravid85@gmail.com>
2025-12-14 13:53:32 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { App } from "./app.component";
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { routes } from './app.routes';
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+  declarations: [App],
+  imports: [BrowserModule, FormsModule],
+
+  providers: [
+
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ],
+  bootstrap: [App]
+
+})
+export class AppModule { }
+
